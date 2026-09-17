e893cb3cf6e399068a63c885aab98e2a317b552c
Vol-Dem <saten898@gmail.com>
2026-09-18 01:19:18 +0300

    refactor: reuse icon components for repeated SVGs

---
+++ src/assets/icons/CivitaiShareIcon.tsx
@@ -0,0 +1,19 @@
+const CivitaiShareIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M13 4v4c-6.575 1.028 -9.02 6.788 -10 12c-.037 .206 5.384 -5.962 10 -6v4l8 -7l-8 -7z"></path>
+    </svg>
+  );
+};
+
+export default CivitaiShareIcon;
--- src/components/ui/WarningMessage.tsx
+++ src/components/ui/WarningMessage.tsx
@@ -6,6 +6,7 @@ import {
 } from "../../variables/constants";
 import classes from "./WarningMessage.module.scss";
 import type { ComponentProps } from "react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 
 type WarningMessageProps = ComponentProps<"div">;
 
@@ -16,19 +17,7 @@ const WarningMessage = ({ children, className }: WarningMessageProps) => {
       animate={ANIMATIONS_FM_SLIDEIN}
       className={`${classes.warning} ${className || ""}`}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
-        />
-      </svg>
+      <ExclamationTriangleIcon />
       <div>{children}</div>
     </motion.div>
   );
